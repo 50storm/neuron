07dee1a30c6f33fe707d6257d8aa31de1d76c8d0
50Storm <hirotech.net@gmail.com>
2018-09-02 15:31:43 +0900

    first commit

---
+++ neuron.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define INPUTNO 2
+#define MAXINPUTNO 100
+
+double f(double u);
+void initw(double w[INPUTNO+1]);
+int main()
+{
+    double w[INPUTNO + 1]; //重みとしきいち
+    double e[MAXINPUTNO][INPUTNO];//データセット
+    double o; // 出力
+
+    int i,j;
+    int n_of_e;
+    initw(w);
+
+}
+
+//学習データ読み込み
+int getdata( double e[][INPUTNO] ){
+  int n_of_e = 0; //学習データセットの個数
+  int j = 0;
+
+  while (scanf("%lf", &e[n_of_e][j]) != EOF) {
+    ++j;
+    if( j>= INPUTNO){
+      j=0;
+      ++n_of_e;
+
+    }
+  }
+
+  return n_of_e;
+}
+
+
+//forward関数
+double forward(double w[INPUTNO+1], double e[INPUTNO])
+{
+  int i;
+  double u,o;
+
+  u = 0;
+  for(i = 0; i<INPUTNO ; ++i){
+    u+=e[i]*w[i];
+  }
+  u-=w[i];  //しきい値の処理
+
+  //出力計算
+  o = f(u);
+
+  return o;
+
+}
+
+//重みの初期化
+void initw(double w[INPUTNO + 1]){
+  w[0] = 1;
+  w[1] = 1;
+  w[2] = 1.5;
+}
+
+
+//伝搬関数
+double f(double u){
+  //ステップ関数
+  if( u>=0 ) return 1.0;
+  else return 0.0;
+
+  //シグモイド関数
+  // return 1.0/(1.0+exp(-u))
+
+}
